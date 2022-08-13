d0bea9a6a2be296f0085afa5517cc71e0cb35093
rodrigoDesktop <rodrigo.abasto21@gmail.com>
2022-08-13 17:33:02 -0400

    Agregando Sanctum

---
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'device_name' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        return response()->json(['access_token' => $user->createToken($request->device_name)->plainTextToken], 200);
+    }
+
+    public function logout()
+    {
+        return request()->user()->currentAccessToken()->delete();
+    }
+
+    /*public function register()
+    {
+        request()->validate([
+            'name' => ['required'],
+            'email' => ['required', 'email', 'unique:users,email'],
+            'password' => ['required'],
+            'device_name' => ['required']
+        ]);
+
+        $user = User::create([
+            'name' => request('name'),
+            'email' => request('email'),
+            'password' => bcrypt(request('password')),
+        ]);
+
+        return $user->createToken(request('device_name'))->plainTextToken;
+    }*/
+}
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/login', 'Api\Auth\AuthController@login');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', 'Api\Auth\AuthController@logout');
+    Route::get('/user', function (Request $request){
+        return $request->user();
+    });
 });
